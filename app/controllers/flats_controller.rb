52d23e2bd302dc3115d14a94e3148227160264ad
David Jungblut <davidobob@laposte.net>
2020-02-19 17:05:26 +0100

    first commit

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,27 @@
+class FlatsController < ApplicationController
+
+def index
+  @flats = Flat.all
+end
+
+def show
+
+end
+
+def new
+
+end
+
+def create
+
+end
+
+def update
+
+end
+
+def destroy
+
+end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating flats"
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Dark & Unspacious grass Flat London',
+  address: '10 Garbage Gardens London W9 1DT',
+  description: 'A really bad summer feel for this unspacious grass flat. Two quarter of bedrooms, closed plan living area, small kitchen and an ugly conservatory',
+  price_per_night: 150,
+  number_of_guests: 1
+)
+
+puts "done"
